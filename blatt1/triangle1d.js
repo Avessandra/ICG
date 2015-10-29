7f653ff97f68f4c5d845d4127f570d32be5ec722
kautschuckflasche <nilsw.agner@web.de>
2015-10-29 16:52:45 +0100

    etwas ORDNUNG und Aufgabe 2 fertig

---
+++ blatt1/triangle1d.js
@@ -0,0 +1,59 @@
+var gl;
+
+window.onload = function init()
+{
+	// Get canvas and setup webGL
+	
+	var canvas = document.getElementById("gl-canvas");
+	gl = WebGLUtils.setupWebGL(canvas);
+	if (!gl) { alert("WebGL isn't available"); }
+
+	// Specify position and color of the vertices
+	
+	var vertices = new Float32Array([	-0.5, 1, 
+										0.5, 1, 
+										0, -1]);
+	var colors = new Float32Array([ 1, 0, 0, 1, 
+									
+									0, 1, 0, 1,
+									1, 1, 0, 1,]);
+
+	// Configure viewport
+
+	gl.viewport(0,0,canvas.width,canvas.height);
+	gl.clearColor(1.0,1.0,1.0,1.0);
+
+	// Init shader program and bind it
+
+	var program = initShaders(gl, "vertex-shader", "fragment-shader");
+
+	gl.useProgram(program);
+	
+	// Load colors into the GPU and associate shader variables
+	
+	var cBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+	
+	var vColor = gl.getAttribLocation(program, "vColor");
+	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(vColor);
+
+	// Load positions into the GPU and associate shader variables
+
+	var bufferId = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+	gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+	var vPosition = gl.getAttribLocation(program, "vPosition");
+	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(vPosition);
+	render();
+};
+
+function render()
+{
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	document.getElementsByTagName('h1')[0].innerHTML = 'Ein Dreieck';
+	gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
